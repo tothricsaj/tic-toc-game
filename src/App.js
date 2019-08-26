b20fe8c43ae06ba09c61764951f041e809304d63
tothricsaj <tothricsaj@gmail.com>
2019-08-26 16:11:35 +0200

    GameFieldBundle is added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import GameField  from './GameFieldBundle/GameField.bundle';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is an tic-toc game example for me
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <GameField />
+      </div>
   );
 }
 
---
+++ src/GameFieldBundle/GameField.bundle.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class GameField extends Component {
+    /*
+    constructor() {
+
+    }
+    */
+
+    render() {
+        return(
+            <div>
+                Szevasz tavasz!!!!
+            </div>
+        );
+    }
+}
+
+export default GameField;
