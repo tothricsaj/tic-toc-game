25885d484e4a1a1b921214034eccba255ff3f321
tothricsaj <tothricsaj@gmail.com>
2019-08-27 15:54:39 +0200

    click to X :)

--- src/GameFieldBundle/GameField.bundle.js
+++ src/GameFieldBundle/GameField.bundle.js
@@ -4,8 +4,16 @@ import style from './GameFieldBundle.module.scss';
 class GameField extends Component {
     constructor(props) {
         super(props);
+        this.drawX = this.drawX.bind(this);
         this.fieldNumbers = [1,2,3,4,5,6,7,8,9];
-        this.fields = this.fieldNumbers.map((num) => <li key={ num }></li> );
+        this.fields = this.fieldNumbers.map((num) => <li id={'field-' + num} onClick={(e) => this.drawX(num, e)} key={ num }></li> );
+    }
+
+    drawX(num) {
+        let elem = document.getElementById('field-' + num);
+        let imgUrl = process.env.PUBLIC_URL + '/cross.png';
+        elem.style.backgroundImage = `url(${imgUrl})`;
+        console.log(process.env.PUBLIC_URL + 'cross.png');
     }
 
     render() {
