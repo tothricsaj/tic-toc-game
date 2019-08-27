b0f61ccb1a96bff3094931812a4d9115e4b87c0a
tothricsaj <tothricsaj@gmail.com>
2019-08-27 13:37:16 +0200

    game filed's view is ready

--- src/GameFieldBundle/GameField.bundle.js
+++ src/GameFieldBundle/GameField.bundle.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import style from './GameFieldBundle.module.scss';
 
 class GameField extends Component {
     constructor(props) {
         super(props);
-        this.fieldNumbers = [1,2,3,4,5,6,8,9];
+        this.fieldNumbers = [1,2,3,4,5,6,7,8,9];
         this.fields = this.fieldNumbers.map((num) => <li key={ num }></li> );
     }
 
     render() {
         return(
             <div>
-                <ul>
+                <ul className={ style.fieldWrapper }>
                     { this.fields }
                 </ul>
             </div>
